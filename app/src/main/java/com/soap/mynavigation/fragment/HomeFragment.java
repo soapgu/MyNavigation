a4e428507560d4743a64b10e0d61aac6537b7900
soapgu <ghost_gu@163.com>
2022-02-28 23:26:46 +0800

    Initial commit

---
+++ app/src/main/java/com/soap/mynavigation/fragment/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.soap.mynavigation.fragment;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.soap.mynavigation.databinding.HomeFragmentBinding;
+import com.soap.mynavigation.viewmodel.HomeViewModel;
+
+public class HomeFragment extends Fragment {
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        HomeFragmentBinding binding = HomeFragmentBinding.inflate(inflater,container,false);
+        HomeViewModel mViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        binding.setDataContext(mViewModel);
+        return binding.getRoot();
+    }
+
+}
---
+++ app/src/main/java/com/soap/mynavigation/viewmodel/HomeViewModel.java
@@ -0,0 +1,10 @@
+package com.soap.mynavigation.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    public String getTitle() {
+        return "this is home page!";
+    }
+}
