714c45b96a4eb937220070a3d6e949ff6df5a87a
soapgu <ghost_gu@163.com>
2022-03-01 14:26:03 +0800

    add BottomNavigationView and logger

--- app/src/main/java/com/soap/mynavigation/MainActivity.java
+++ app/src/main/java/com/soap/mynavigation/MainActivity.java
@@ -1,8 +1,14 @@
 package com.soap.mynavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.orhanobut.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+        navController.addOnDestinationChangedListener( (controller,destination,arguments) ->{
+            Logger.i( "DestinationChanged:%s",destination.getDisplayName());
+            switch (destination.getId() ) {
+                case R.id.homeFragment:
+                    Logger.i("go to home");
+                    break;
+                case R.id.spaceFragment:
+                    Logger.i("go to space");
+                    break;
+                case R.id.myFragment:
+                    Logger.i("go to my");
+                    break;
+                default:
+                    Logger.i("other...");
+            }
+        });
+
     }
 }
